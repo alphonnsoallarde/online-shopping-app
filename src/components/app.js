141957be40b4065850e69994537590e9bdedd44f
alphonnsoallarde <alphonnsoallarde@gmail.com>
2017-07-17 23:32:46 +0800

    Add partials - header and footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from './partials/header';
+import Footer from './partials/footer';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Home Page</h1>
-        <h2>This is the landing page!</h2>
-        <ul>
-          <li><a href="/login">Login Page</a></li>
-        </ul>
+        <Header />
+          <div>
+            <h1>Home Page</h1>
+            <h2>This is the landing page!</h2>
+            <ul>
+              <li><a href="/login">Login Page</a></li>
+            </ul>
+          </div>
+        <Footer />
       </div>
     );
   }
